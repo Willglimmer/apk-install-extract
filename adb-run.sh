053821b8d090a998571e17d22a89227eba0d0298
Will <iwillbe668@gmail.com>
2025-12-12 20:31:32 +0800

    fixed workflow

--- adb-run.sh
+++ adb-run.sh
@@ -3,4 +3,8 @@ ver=$(aapt dump badging $1 | awk -F " " '/package/ {print $4}' | awk -F "'" '/ve
 act=$(aapt dump badging $1|awk -F" " '/launchable-activity/ {print $2}'|awk -F"'" '/name=/ {print $2}')
 pkg_ver=$( echo "$pkg.$ver")
 echo "Running package: $pkg ver: $ver with activity: $act"
+if [ -z "$act" ]; then
+  echo "No launchable activity found for $pkg; skipping start."
+  exit 0
+fi
 adb shell am start -n $pkg/$act
